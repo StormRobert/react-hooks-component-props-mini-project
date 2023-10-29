453b739a8a3c93e039fb8e519390798ebeeb63df
storm <stormarasa2@gmail.com>
2023-10-29 17:48:31 +0300

    final commit

--- src/data/blog.js
+++ src/data/blog.js
@@ -1,21 +1,21 @@
 import logo from "../assets/logo";
 
 const blogData = {
-  name: "Underreacted",
+  name: "Storm Underreacted",
   image: logo,
   about: "A blog about learning React",
   posts: [
     {
       id: 1,
       title: "Components 101",
-      date: "December 15, 2020",
+      date: "December 26, 2023",
       preview: "Setting up the building blocks of your site",
       minutes: 5,
     },
     {
       id: 2,
       title: "React Data Flow",
-      date: "December 11, 2020",
+      date: "December 26, 2023",
       preview: "Passing props is never passé",
       minutes: 15,
     },
